cfd29d84f1cee5f24c8e62892197e575608b25bc
Jingyi Xu <jx15@ualberta.ca>
2025-09-27 02:59:40 -0600

    TDD pre

--- src/main/java/city/service/Utility.java
+++ src/main/java/city/service/Utility.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utility {
-    public List<Address> getAddressesByCity(City city, String cityName) {
-        List<Address> addresses = new ArrayList<>();
-        List<Address> cityAddresses = city.listAddresses();
-
-        for (int i = 0; i < cityAddresses.size(); i++) {
-            if (cityAddresses.get(i).city.equals(cityName)) {
-                addresses.add(cityAddresses.get(i));
-            }
-        }
-        return addresses;
-    }
+//    public List<Address> getAddressesByCity(City city, String cityName) {
+//        List<Address> addresses = new ArrayList<>();
+//        List<Address> cityAddresses = city.listAddresses();
+//
+//        for (int i = 0; i < cityAddresses.size(); i++) {
+//            if (cityAddresses.get(i).city.equals(cityName)) {
+//                addresses.add(cityAddresses.get(i));
+//            }
+//        }
+//        return addresses;
+//    }
 
     public List<Integer> getSalariesOver(Salary employeeSalary, Integer filter) {
         List<Integer> salaries = employeeSalary.returnSalaries();
--- src/test/java/city/service/UtilityTest.java
+++ src/test/java/city/service/UtilityTest.java
@@ -9,26 +9,26 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UtilityTest {
-    @Test
-    public void getAddresses() {
-        City city = mock(City.class);// mocking an interface
-        List<Address> addresses = new ArrayList<>();
-        addresses.add(new Address("address1", "city1"));
-        addresses.add(new Address("address2", "city1"));
-        addresses.add(new Address("address3", "city1"));
-        addresses.add(new Address("address4", "city2"));
-        when(city.listAddresses()).thenReturn(addresses);
-        Utility utility = new Utility();
-        List<Address> correctAddresses = new ArrayList<>();
-        correctAddresses.add(new Address("address1", "city1"));
-        correctAddresses.add(new Address("address2", "city1"));
-        correctAddresses.add(new Address("address3", "city1"));
-        List<Address> testAddresses = utility.getAddressesByCity(city, "city 1");
-        for (int i = 0; i < testAddresses.size(); i++) {
-            assertEquals(testAddresses.get(i).address,
-                    correctAddresses.get(i).address);
-        }
-    }
+//    @Test
+//    public void getAddresses() {
+//        City city = mock(City.class);// mocking an interface
+//        List<Address> addresses = new ArrayList<>();
+//        addresses.add(new Address("address1", "city1"));
+//        addresses.add(new Address("address2", "city1"));
+//        addresses.add(new Address("address3", "city1"));
+//        addresses.add(new Address("address4", "city2"));
+//        when(city.listAddresses()).thenReturn(addresses);
+//        Utility utility = new Utility();
+//        List<Address> correctAddresses = new ArrayList<>();
+//        correctAddresses.add(new Address("address1", "city1"));
+//        correctAddresses.add(new Address("address2", "city1"));
+//        correctAddresses.add(new Address("address3", "city1"));
+//        List<Address> testAddresses = utility.getAddressesByCity(city, "city 1");
+//        for (int i = 0; i < testAddresses.size(); i++) {
+//            assertEquals(testAddresses.get(i).address,
+//                    correctAddresses.get(i).address);
+//        }
+//    }
 
     @Test
     public void testSalaryFiltering() {
